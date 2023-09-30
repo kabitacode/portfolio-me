3769f29ef8e92ccede6cca259027b69361ec1114
Muhamad Zulfiqor <siapaa249@gmail.com>
2023-09-30 22:24:23 +0700

    ✨ feat : add section headeing

---
+++ components/section-heading.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+
+type Props = {
+    children: React.ReactNode
+}
+
+ const SectionHeading = ({children} : Props) => {
+  return (
+    <h2 className='text-3xl font-medium capitalize mb-8'>{children}</h2>
+  )
+}
+
+export default SectionHeading
