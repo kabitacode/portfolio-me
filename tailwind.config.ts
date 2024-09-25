cd3a28b2062610c28b0ae933561e59667e30688f
Muhamad Zulfiqor <siapaa249@gmail.com>
2024-09-25 09:04:16 +0700

    config tailwind

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -60,6 +60,16 @@ const config = {
         sm: "calc(var(--radius) - 4px)",
       },
       keyframes: {
+        orbit: {
+          "0%": {
+            transform:
+              "rotate(0deg) translateY(calc(var(--radius) * 1px)) rotate(0deg)",
+          },
+          "100%": {
+            transform:
+              "rotate(360deg) translateY(calc(var(--radius) * 1px)) rotate(-360deg)",
+          },
+        },
         "border-beam": {
           "100%": {
             "offset-distance": "100%",
@@ -83,6 +93,7 @@ const config = {
         },
       },
       animation: {
+        orbit: "orbit calc(var(--duration)*1s) linear infinite",
         "border-beam": "border-beam calc(var(--duration)*1s) infinite linear",
         meteor: "meteor 5s linear infinite",
         "accordion-down": "accordion-down 0.2s ease-out",
