eb4078c2b316f47bc58b07dd3b0b886f291cd219
Muhamad Zulfiqor <siapaa249@gmail.com>
2025-05-16 22:50:28 +0700

    ✨  update skills

--- components/skills.tsx
+++ components/skills.tsx
@@ -30,6 +30,29 @@ export default function Skils() {
         >
             <SectionHeading>My Skills</SectionHeading>
 
+
+            <div className="flex md:flex-row flex-wrap md:items-center">
+                <Link href={'https://reactnative.dev'} target='_blank'>
+                    <div className="bg-white dark:bg-blue-300 w-16 h-16 flex py-2 md:p-4 m-2 rounded-full shadow-md items-center justify-center transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 hover:bg-blue-300 duration-300">
+                        <SiReact size={getSize()} />
+                    </div>
+                </Link>
+                <Link href={'https://flutter.dev/'} target='_blank'>
+                    <div className="bg-white dark:bg-green-800 w-16 h-16 flex py-2 md:p-4 m-2 rounded-full shadow-md items-center justify-center transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 hover:bg-green-300 duration-300">
+                        <SiFlutter size={getSize()} />
+                    </div>
+                </Link>
+                <Link href={'https://nextjs.org/'} target='_blank'>
+                    <div className="bg-white dark:bg-blue-300 w-16 h-16 flex py-2 md:p-4 m-2 rounded-full shadow-md items-center justify-center transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 hover:bg-orange-300 duration-300">
+                        <SiNextdotjs size={getSize()} />
+                    </div>
+                </Link>
+                <Link href={'https://redux.js.org/'} target='_blank'>
+                    <div className="bg-white dark:bg-indigo-400 w-16 h-16 flex py-2 md:p-4 m-2 rounded-full shadow-md items-center justify-center transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 hover:bg-indigo-400 duration-300">
+                        <SiRedux size={getSize()} />
+                    </div>
+                </Link>
+            </div>
             <div className="flex md:flex-row flex-wrap md:items-center">
                 <div className="bg-white dark:bg-red-500 w-16 h-16 flex py-2 md:p-4 m-2 rounded-full shadow-md items-center justify-center transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 hover:bg-orange-400 duration-300">
                     <IoLogoHtml5 size={getSize()} />
@@ -43,24 +66,6 @@ export default function Skils() {
                 <Link href='https://www.typescriptlang.org/' target='_blank'>
                     <div className="bg-white dark:bg-indigo-300 w-16 h-16 flex py-2 md:p-4 mb-4 m-2 rounded-full shadow-md items-center justify-center transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 hover:bg-indigo-300 duration-300">
                         <SiTypescript size={getSize()} />
-                    </div>
-                </Link>
-            </div>
-            <div className="flex md:flex-row flex-wrap md:items-center">
-
-                <Link href={'https://redux.js.org/'} target='_blank'>
-                    <div className="bg-white dark:bg-indigo-400 w-16 h-16 flex py-2 md:p-4 m-2 rounded-full shadow-md items-center justify-center transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 hover:bg-indigo-400 duration-300">
-                        <SiRedux size={getSize()} />
-                    </div>
-                </Link>
-                <Link href={'https://reactnative.dev'} target='_blank'>
-                    <div className="bg-white dark:bg-blue-300 w-16 h-16 flex py-2 md:p-4 m-2 rounded-full shadow-md items-center justify-center transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 hover:bg-blue-300 duration-300">
-                        <SiReact size={getSize()} />
-                    </div>
-                </Link>
-                <Link href={'https://nextjs.org/'} target='_blank'>
-                    <div className="bg-white dark:bg-blue-300 w-16 h-16 flex py-2 md:p-4 m-2 rounded-full shadow-md items-center justify-center transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 hover:bg-blue-300 duration-300">
-                        <SiNextdotjs size={getSize()} />
                     </div>
                 </Link>
             </div>
