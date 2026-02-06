5ef4d36662201335f47d1713fadc661057003f30
Muhamad Zulfiqor <siapaa249@gmail.com>
2026-02-06 20:04:02 +0700

    ✨ feat : add use client in footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import React from "react";
 
 export default function Footer() {
@@ -6,7 +8,7 @@ export default function Footer() {
   return (
     <footer className="mb-10 px-4 text-center text-gray-500">
       <small className="mb-2 block text-xs">
-        &copy; {currentYear} Muhamad Zulfiqor. All rights reserved.
+        &copy; {currentYear} Muhamad . All rights reserved.
       </small>
       <p className="text-xs">
         <span className="font-semibold">About this website:</span> built with
